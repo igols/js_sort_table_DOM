c791b7484e794a9f407a0a6850f5ceb196cd1f5a
Igor <igor.olshanskyi@gmail.com>
2024-11-14 13:54:09 +0200

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 // write code here
+
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+
+const th = Array.from(document.querySelectorAll('thead > tr > th'));
+
+thead.addEventListener('click', (e) => {
+  const index = th.indexOf(e.target);
+
+  const rowSort = [...tbody.rows].sort((a, b) => {
+    return a.children[index].innerText - b.children[index].innerText;
+  });
+
+  tbody.append(...rowSort);
+});
